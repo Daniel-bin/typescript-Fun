61e29fb97fa4993b870b84050568c87afe64e6b8
Daniel-bin <daniel.coughlan@outlook.ie>
2020-10-06 23:15:24 +0100

    1 file modified

--- index.ts
+++ index.ts
@@ -5,4 +5,14 @@ import './style.css';
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
 
-let myVar
+let age : number;
+age = 3;
+
+let name : string;
+name = "Joe";
+
+let adult : boolean;
+adult = true;
+adult = 3;
+
+let result : any;
